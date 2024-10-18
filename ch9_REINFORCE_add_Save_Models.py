2e5681b2344255a048cf2dfefb3b5b50ea251b73
DiggerLaboratory <diggerlaboratory@gmail.com>
2024-10-18 20:22:47 +0900

    chat 9 reinforce and actor critic

---
+++ ch9_REINFORCE_add_Save_Models.py
@@ -0,0 +1,66 @@
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+import os
+#Hyperparameters
+learning_rate = 0.0002
+gamma         = 0.98
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.data = []
+        self.fc1 = nn.Linear(4, 128)
+        self.fc2 = nn.Linear(128, 2)
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        os.makedirs("./policy/Reinforce",exist_ok=True)
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=0)
+        return x
+      
+    def put_data(self, item):
+        self.data.append(item)
+        
+    def train_net(self):
+        R = 0
+        self.optimizer.zero_grad()
+        for r, prob in self.data[::-1]:
+            R = r + gamma * R
+            loss = -torch.log(prob) * R
+            loss.backward()
+        self.optimizer.step()
+        self.data = []
+
+def main():
+    env = gym.make('CartPole-v1')
+    pi = Policy()
+    score = 0.0
+    print_interval = 20
+    best_score = 0
+    for n_epi in range(10000):
+        s, _ = env.reset()
+        done = False
+        
+        while not done: # CartPole-v1 forced to terminates at 500 step.
+            prob = pi(torch.from_numpy(s).float())
+            m = Categorical(prob)
+            a = m.sample()
+            s_prime, r, done, truncated, info = env.step(a.item())
+            pi.put_data((r,prob[a]))
+            s = s_prime
+            score += r
+        pi.train_net()
+        if n_epi%print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {}".format(n_epi, score/print_interval))
+            if score/print_interval > best_score:
+                best_score = score/print_interval
+                torch.save(obj=pi.state_dict(),f=f"./policy/Reinforce/Reinforce_BestModel_epi_{str(n_epi).zfill(5)}_{best_score}.pth")
+            score = 0.0
+    env.close()
+    
+if __name__ == '__main__':
+    main()
