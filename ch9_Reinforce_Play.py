8d7434e032c44611069a424245774590c19c1701
DiggerLaboratory <diggerlaboratory@gmail.com>
2024-10-18 21:09:46 +0900

    a

---
+++ ch9_Reinforce_Play.py
@@ -0,0 +1,16 @@
+from ch9_REINFORCE import Policy
+import gym
+import torch
+from torch.distributions import Categorical
+if __name__=="__main__":
+    env = gym.make('CartPole-v1')
+    pi = Policy()
+    pi.load_state_dict(torch.load("/home/ssu20/rlDir/policy/Reinforce/Reinforce_BestModel_epi_03820_1333.65.pth"))
+    s, _ = env.reset()
+    done = False
+    while not done: # CartPole-v1 forced to terminates at 500 step.
+        prob = pi(torch.from_numpy(s).float())
+        m = Categorical(prob)
+        a = m.sample()
+        s_prime, r, done, truncated, info = env.step(a.item())
+        s = s_prime
